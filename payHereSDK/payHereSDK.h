556856ee5f34599780ae058a05df8e0b7056c93a
kamalupasena <upasenas@gmail.com>
2018-03-09 15:08:33 +0530

    Initial Commit

---
+++ payHereSDK/payHereSDK.h
@@ -0,0 +1,19 @@
+//
+//  payHereSDK.h
+//  payHereSDK
+//
+//  Created by Kamal Sampath Upasena on 3/9/18.
+//  Copyright © 2018 PayHere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for payHereSDK.
+FOUNDATION_EXPORT double payHereSDKVersionNumber;
+
+//! Project version string for payHereSDK.
+FOUNDATION_EXPORT const unsigned char payHereSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <payHereSDK/PublicHeader.h>
+
+
