5adcd818c0cf5b5427b865cce529711bc79becc8
kamalsampathupasena <kamal@bhasha.lk>
2021-08-25 11:31:09 +0530

    Remove Sandbox Env. TEST Method Change On Completion URL on Sandbox

--- payHereSDK/Sources/PHBottomViewController.swift
+++ payHereSDK/Sources/PHBottomViewController.swift
@@ -128,11 +128,11 @@ internal class PHBottomViewController: UIViewController {
             self.collectionView.isHidden = true
             self.progressBar.isHidden = true
             self.selectedPaymentOption = PaymentOption(name: "Visa", image: getImage(withImageName: "visa"), optionValue: "VISA")
-            if(isSandBoxEnabled){
-                self.startProcess(paymentMethod: "TEST")
-            } else {
+//            if(isSandBoxEnabled){
+//                self.startProcess(paymentMethod: "TEST")
+//            } else {
                 self.startProcess(paymentMethod: "VISA")
-            }
+//            }
             self.lblselectedMethod.text = "Credit/Debit Card"
         }
         
@@ -855,7 +855,7 @@ extension PHBottomViewController : WKUIDelegate,WKNavigationDelegate{
     internal func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         
         
-        if((navigationAction.request.mainDocumentURL?.absoluteString.contains("https://www.payhere.lk/pay/payment/complete"))! || (navigationAction.request.mainDocumentURL?.absoluteString.contains("https://sandbox.payhere.lk/pay/status/test"))!){
+        if((navigationAction.request.mainDocumentURL?.absoluteString.contains("https://www.payhere.lk/pay/payment/complete"))! || (navigationAction.request.mainDocumentURL?.absoluteString.contains("https://sandbox.payhere.lk/pay/status/test"))! || (navigationAction.request.mainDocumentURL?.absoluteString.contains("https://sandbox.payhere.lk/pay/payment/complete"))!){
             if(self.initRepsonse?.data?.order != nil){
                 if isSandBoxEnabled{
                     
@@ -960,11 +960,11 @@ extension PHBottomViewController :  UICollectionViewDelegate, UICollectionViewDa
         let payOption = paymentOption[indexPath.section].1[indexPath.row]
         
         self.selectedPaymentOption = payOption
-        if(self.isSandBoxEnabled){
-            startProcess(paymentMethod: "TEST")
-        }else{
+//        if(self.isSandBoxEnabled){
+//            startProcess(paymentMethod: "TEST")
+//        }else{
             startProcess(paymentMethod: payOption.optionValue)
-        }
+//        }
         
         self.viewNavigationWrapper.isHidden = false
         self.lblMethodPrecentTitle.isHidden = true
