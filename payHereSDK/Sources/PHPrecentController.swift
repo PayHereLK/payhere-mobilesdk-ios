be5d333d03517490ed14781d97d6933e9bc902f7
Prashan Samarathunge <prashansamarathunge@gmail.com>
2025-05-09 13:15:17 +0530

    (#20)   refactor: renamed `precent` to `present` for clarity and consistency  Marked `precent` as deprecated; retained for backward compatibility.

--- payHereSDK/Sources/PHPrecentController.swift
+++ payHereSDK/Sources/PHPrecentController.swift
@@ -10,19 +10,7 @@ import Foundation
 
 public class PHPrecentController{
     
-//    @available(*, deprecated, message: "Use method signature with InitResonseRequest")
-//    public static func precent(form : UIViewController,isSandBoxEnabled sandBoxEnabled: Bool,withInitRequest request : InitRequest,delegate : PHViewControllerDelegate){
-//        
-//        let phVC = PHViewController()
-//        phVC.initRequest = request
-//        phVC.delegate = delegate
-//        phVC.isSandBoxEnabled = false
-//        phVC.modalPresentationStyle = .overFullScreen
-//        
-//        form.present(phVC, animated: true, completion: nil)
-//        
-//    }
-    
+    @available(*, deprecated, renamed: "present", message: "Use present(from:,withInitRequest:,shouldShowPaymentStatus:,delegate) instead!")
     public static func precent(from : UIViewController,
                                withInitRequest request : PHInitialRequest,
                                shouldShowPaymentStatus showPaymentStatus:Bool = true,
@@ -58,4 +46,42 @@ public class PHPrecentController{
             
         }
     }
+    
+    public static func present(from : UIViewController,
+                               withInitRequest request : PHInitialRequest,
+                               shouldShowPaymentStatus showPaymentStatus:Bool = true,
+                               delegate : PHViewControllerDelegate){
+        
+        let bundle = Bundle.payHereBundle
+        let storyBoard: UIStoryboard = UIStoryboard(name: "PayHere", bundle: bundle)
+        if let initialController = storyBoard.instantiateViewController(withIdentifier: "PHBottomViewController") as? PHBottomViewController{
+            
+            var isSandBoxEnabled : Bool  = false
+            
+            if (request.merchantID?.starts(with: "1"))!{
+                isSandBoxEnabled = true
+            }else{
+                isSandBoxEnabled = false
+            }
+            
+            
+            print(isSandBoxEnabled)
+            
+            initialController.initialRequest = request
+            initialController.isSandBoxEnabled = isSandBoxEnabled
+            initialController.shouldShowSucessView = showPaymentStatus
+            initialController.delegate = delegate
+        
+            from.modalPresentationStyle = .overCurrentContext
+            from.modalTransitionStyle = .crossDissolve
+            initialController.modalPresentationStyle = .overCurrentContext
+            initialController.modalTransitionStyle = .crossDissolve
+            
+            from.present(initialController, animated: true, completion: nil)
+                
+            
+        }
+    }
+    
+    
 }
